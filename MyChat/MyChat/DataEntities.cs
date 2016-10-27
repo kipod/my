e327bfc0c53b43ae36de65540e1dab149382ea4d
Nikolay Chernov <n_chernov@wargaming.net>
2016-10-27 19:07:47 +0300

    MyChat project. Init commit

---
+++ MyChat/MyChat/DataEntities.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Entity;
+using MySql.Data.Entity;
+using MySql.Data.MySqlClient;
+
+namespace MyChat
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string MachineName { get; set; }
+    }
+
+    public class Message
+    {
+        public virtual User User { get; set; }
+        public string Text { get; set; }
+    }
+
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class ChatContext : DbContext
+    {
+        public ChatContext()
+            : base(new MySqlConnection("server=BY1-WWV-220;port=3306;database=MyChat;uid=User;password=12345678"), false)
+        {
+            Database.CreateIfNotExists();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().MapToStoredProcedures();
+            modelBuilder.Entity<Message>().MapToStoredProcedures();
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
---
+++ MyChat/MyChat/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyChat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                using (var db = new ChatContext())
+                {
+                    var load = from m in db.Messages select m;
+                    dataGrid.ItemsSource = load;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null) MessageBox.Show(ex.InnerException.ToString());
+                throw;
+            }
+        }
+    }
+}
